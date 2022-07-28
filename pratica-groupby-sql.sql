1c5c72f6903658a20d2b7e4b266c33eeebfad37d
Lucas Duarte <lukazds2016@gmail.com>
2022-07-28 15:31:24 -0300

    Refactoring code

--- pratica-groupby-sql.sql
+++ pratica-groupby-sql.sql
@@ -1,22 +1,29 @@
 -- QUESTION 1:
 
-SELECT COUNT("endDate") AS "currentExperiences" FROM experiences;
+SELECT COUNT("endDate") AS "currentExperiences" 
+FROM experiences;
 
 -- QUESTION 2:
 
-SELECT "userId" AS id, COUNT("courseId") AS educations FROM educations GROUP BY "userId";
+SELECT "userId" AS id, COUNT("courseId") AS educations 
+FROM educations 
+GROUP BY "userId";
 
 -- QUESTION 3:
 
 SELECT users.name AS writer, COUNT(testimonials."writerId") AS "testimonailCount" 
-FROM users JOIN (SELECT * FROM testimonials WHERE testimonials."writerId" = 435) testimonials 
-ON users.id = testimonials."writerId" GROUP BY users.name;
-
+FROM users 
+JOIN testimonials 
+ON users.id = testimonials."writerId" 
+WHERE testimonials."writerId" = 435 
+GROUP BY users.name; 
 
 -- QUESTION 4:
 
 SELECT MAX(jobs.salary) AS maximumSalary, roles.name AS role 
-FROM jobs JOIN roles ON roles.id = jobs."roleId" 
+FROM jobs 
+JOIN roles 
+ON roles.id = jobs."roleId" 
 WHERE jobs.active = true 
 GROUP BY roles.name 
 ORDER BY maximumSalary;

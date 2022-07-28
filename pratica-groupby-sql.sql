36a78034160880c3c74683656f28e380de955d18
Lucas Duarte <lukazds2016@gmail.com>
2022-07-28 14:34:45 -0300

    Finishing question three

--- pratica-groupby-sql.sql
+++ pratica-groupby-sql.sql
@@ -8,4 +8,10 @@ SELECT "userId" AS id, COUNT("courseId") AS educations FROM educations GROUP BY 
 
 -- QUESTION 3:
 
+SELECT users.name AS writer, COUNT(testimonials."writerId") AS "testimonailCount" 
+FROM users JOIN (SELECT * FROM testimonials WHERE testimonials."writerId" = 435) testimonials 
+ON users.id = testimonials."writerId" GROUP BY users.name;
+
+
 -- QUESTION 4:
+

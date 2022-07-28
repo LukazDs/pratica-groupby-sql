98241b7188c91c1160b38114db85d1a1a5c56407
Lucas Duarte <lukazds2016@gmail.com>
2022-07-28 11:30:48 -0300

    Finishing question two

--- pratica-groupby-sql.sql
+++ pratica-groupby-sql.sql
@@ -4,6 +4,8 @@ SELECT COUNT("endDate") AS "currentExperiences" FROM experiences;
 
 -- QUESTION 2:
 
+SELECT "userId" AS id, COUNT("courseId") AS educations FROM educations GROUP BY "userId";
+
 -- QUESTION 3:
 
 -- QUESTION 4:
